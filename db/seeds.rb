c79f84db80dab4399d8a5a2a9d6dc75668e78bbe
asagar <asagar@qatar.cmu.edu>
2017-11-19 21:04:29 +0300

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(fname: "Eamon",lname: "Sager", email: "minbayti@cmu.com", password: "Minbayti123", password_confirmation: "Minbayti123",admin: true)
